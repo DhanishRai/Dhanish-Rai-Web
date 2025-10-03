9625f5a9a885d702d3f2e242c60245858ead6a7b
DhanishRai <dhanishraik111@gmail.com>
2025-10-03 22:47:21 +0530

    adjust script.js

---
+++ JAVA SCRIPT/intro to java script/script.js
@@ -0,0 +1,15 @@
+alert("Hello world")
+
+console.log("Code is running ...")
+
+var a = prompt("Enter your number")
+console.log("Your number is "+a)
+
+
+var isTrue=confirm("Are you sure you want to quit this page?")
+if(isTrue){
+    console.log("Computer is blasting" )
+} 
+else{
+    console.log("Computer is not blasting")
+}
